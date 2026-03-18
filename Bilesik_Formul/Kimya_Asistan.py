3c1a8707ce285dcdf75a05bc22e240560dc49a71
abdullahyildirimer/Kimyabdullah <abdullahyildirimer.ay@gmail.com>
2026-03-18 18:44:40 +0300

    Oluşuturma güncellendi

--- Bilesik_Formul/Kimya_Asistan.py
+++ Bilesik_Formul/Kimya_Asistan.py
@@ -907,7 +907,8 @@ st.markdown("""
         box-shadow: 0 0 15px #00ffcc;
     }
     </style>
-    """, unsafe_allow_html=True)
+    """, unsafe_allow_html=True
+)
 
 # Başlık
 st.title("🧪 Kimya Asistan v1.5.0")
